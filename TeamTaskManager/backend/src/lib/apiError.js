4efc73c9d18c8940151fe9c983b26f46cc663496
Shobhit Raj <135508219+lucifer9973@users.noreply.github.com>
2026-05-09 10:23:29 +0530

    Delete TeamTaskManager directory

--- TeamTaskManager/backend/src/lib/apiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-  constructor(statusCode, code, message) {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
-  }
-}
-
-module.exports = { ApiError };
